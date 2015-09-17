71e6781572f54692851eb31774f1e9ee1e00f97e
Efren <efren@novotorium.com>
2015-09-17 16:46:23 -0600

    Welcome page

--- www/js/app.js
+++ www/js/app.js
@@ -5,10 +5,22 @@
  * more tutorials: hollyschinsky.github.io
  */
 var starter = angular.module('starter', ['ionic', 'ngCordova', 'starter.services'])
-.run(function(PushProcessingService) {
+//.run(function(PushProcessingService) {
    //run once for the app
-   PushProcessingService.initialize();
-});
+   //PushProcessingService.initialize();
+//});
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  .state('welcome', {
+    url: '/welcome',
+    templateUrl: 'welcome.html'
+  })
+  .state('login', {
+    url: '/login',
+    templateUrl: 'login.html'
+  });
+  $urlRouterProvider.otherwise('/welcome');
+})
 
 
 

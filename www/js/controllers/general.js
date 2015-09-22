13d598a7448b03dc2469379696c2f9ba9bc68042
Efren <efren@novotorium.com>
2015-09-22 11:17:08 -0600

    Display the correct footer depending on which view is active

--- www/js/controllers/general.js
+++ www/js/controllers/general.js
@@ -2,9 +2,10 @@
 starter.controller('GeneralCtrl',function($scope,$ionicPlatform,$rootScope){
   $scope.showPrivacy = true;
   $rootScope.$on('$ionicView.beforeEnter',function(event,view){
-    console.log(view.stateName);
     if(view.stateName == 'member-home'){
       $scope.showPrivacy = false;
+    } else {
+      $scope.showPrivacy = true;
     }
   });
 

42a6de8b402ac900bc33ef3aa9fd6bfe0c6f68f2
Efren <efren@novotorium.com>
2015-11-02 11:58:40 -0700

    Innappbrowser plugin finally works in ios and android

--- www/js/app.js
+++ www/js/app.js
@@ -10,6 +10,24 @@ var starter = angular.module('starter', ['ionic','ngCordova','starter.services']
   //run once for the app
   //PushProcessingService.initialize();
 //});
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.InAppBrowser) {
+      window.open = cordova.InAppBrowser.open;
+    }
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      // Set the statusbar to use the default style, tweak this to
+      // remove the status bar on iOS or change it to use white instead of dark colors.
+      StatusBar.styleDefault();
+    }
+
+  });
+})
 .config(function($ionicConfigProvider, $stateProvider, $urlRouterProvider) {
   $ionicConfigProvider.navBar.alignTitle('center');
   $stateProvider

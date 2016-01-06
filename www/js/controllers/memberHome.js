f3db959349bf735f9ff15eae3d8e4b55463ee3d7
Efren Carbajal <efren@novotorium.com>
2016-01-05 17:04:48 -0700

    New design member home page

--- www/js/controllers/memberHome.js
+++ www/js/controllers/memberHome.js
@@ -1,4 +1,11 @@
 
 PsychicSource.controller('MemberHomeCtrl',function($scope,AuthService, SummaryService, summary){
 	$scope.summary = summary;
+
+  $scope.refresh = function(){
+    SummaryService.getSummary().then(function(summary){
+       $scope.summary = summary;
+       $scope.apply();
+    });
+  };
 });

a7ddb5fb7f243a6b4b59aa22567e942e955c41f7
Efren Carbajal <efren@novotorium.com>
2016-01-18 16:43:48 -0700

    Reset functionality to cancel not commited availability changes

--- www/js/controllers/availability.js
+++ www/js/controllers/availability.js
@@ -1,14 +1,12 @@
 
-PsychicSource.controller('AvailabilityCtrl',function($scope,AuthService, AvailabilityService, availabilityObj, SummaryService,$timeout,_){
+PsychicSource.controller('AvailabilityCtrl',function($scope,AuthService, AvailabilityService, availabilityObj, SummaryService,$timeout,_,$state,$ionicLoading){
+  $scope.showPhoneDropdown = true;
 	$scope.hours = availabilityObj.hours;
   $scope.countryCodes = availabilityObj.countryCodes;
   SummaryService.info_member();
   $scope.summary = SummaryService.summaryObj();
   $scope.phone = $scope.summary.phone;
   $scope.times = {};
-  $scope.times.countryObj = _.find($scope.countryCodes,function(obj){
-    return obj.CountryID === $scope.summary.countryId;
-  });
   $scope.times.formattedPhone = null;
   $scope.times.availableHours = moment.duration(parseInt($scope.summary.availabilityInSeconds,10),'seconds').format('h:mm');
   if($scope.times.availableHours){
@@ -16,12 +14,27 @@ PsychicSource.controller('AvailabilityCtrl',function($scope,AuthService, Availab
     $scope.times.hour = hoursArr[0];
   }
 
-  $timeout(function(){
-    $scope.times.formattedPhone = $('.intl-tel-input').find('input').first().val();
-    $('.selected-flag').find('.iti-flag').removeClass().addClass('iti-flag').addClass($scope.times.countryObj.CountryCode.toLowerCase());
-  });
 
-  $scope.updateAvailableHours = function(){
+  $scope.afterPageRender = function(){
+    $scope.setFormattedPhone();
+    $scope.setFlag();
+  };
+
+  $scope.setFormattedPhone = function(){
+    $scope.times.formattedPhone = $('input[international-phone-number').val();
+  };
+
+  $scope.setFlag = function(){
+    $('.selected-flag').find('.iti-flag').removeClass().addClass('iti-flag').addClass($scope.times.countryObj.CountryCode.toLowerCase());
+  };
+
+  ($scope.setCountryObj =  function(){
+    $scope.times.countryObj = _.find($scope.countryCodes,function(obj){
+      return obj.CountryID === $scope.summary.countryId;
+    });
+  })();
+ 
+  $scope.update = function(){
     var hour;
     hour = moment.duration(parseInt($scope.times.hour,10),'hours').format('h:mm');
     if(hour){
@@ -30,8 +43,44 @@ PsychicSource.controller('AvailabilityCtrl',function($scope,AuthService, Availab
       $scope.times.hour = hoursArr[0];
       return true;
     }
+    $scope.times.countryObj = _.find($scope.countryCodes,function(obj){
+      return obj.CountryCode === $scope.summary.countryId;
+    });
+    
   };
 
+  $scope.getCountryCodeSelected = function(){
+    var classesStr = $('.selected-flag').find('.iti-flag').attr('class');
+    var classesArr = classesStr.split(" ");
+    return classesArr[1].toUpperCase();
+  };
+
+  $scope.reset = function(){
+    $ionicLoading.show({template: 'Reseting Info...'});
+    $('input[international-phone-number]').on("destroyed",function(){
+      console.log('input removed');
+    });
+    $scope.showPhoneDropdown = false;
+    if($scope.times.availableHours){
+      var hoursArr = $scope.times.availableHours.split(":");
+      $scope.times.hour = hoursArr[0];
+    }
+    $scope.setCountryObj();
+    $timeout(function(){ 
+      insertionQ('input[international-phone-number]').every(function() {
+        $timeout(function(){
+          $scope.afterPageRender();
+          $ionicLoading.hide();
+        },500);
+      });
+      $scope.showPhoneDropdown = true;
+    },500);
+  };
+
+  $timeout(function(){
+    $scope.afterPageRender();
+  });
+  
   //$scope.refresh = function(){
     //SummaryService.getSummary().then(function(summary){
        //$scope.summary = summary;

737bc10ab7b5e67baae1816dc2872d221a405790
Efren Carbajal <efren@novotorium.com>
2016-01-20 10:44:56 -0700

    Added navigation links to Availability Setup Page

--- www/js/controllers/memberHome.js
+++ www/js/controllers/memberHome.js
@@ -1,6 +1,8 @@
 
 PsychicSource.controller('MemberHomeCtrl',function($scope,AuthService, SummaryService, summary){
 	$scope.summary = summary;
+  $scope.date = {};
+  $scope.date.availabilityUntil = new moment().tz('EST').add(parseInt(summary.availabilityInSeconds,10),'seconds').format('ha z M/D/YYYY')
 
   $scope.refresh = function(){
     SummaryService.getSummary().then(function(summary){

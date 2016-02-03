5273c9e49f58c52c603e5027edb63a80dda79d37
Efren Carbajal <efren@novotorium.com>
2016-02-03 09:24:31 -0700

    Feedback from stakeholders

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,5 @@
 
-var PsychicSource = angular.module('PsychicSource', ['ionic','ionic.utils','ngCordova','PsychicSource.Authentication', 'PsychicSource.Summary', 'PsychicSource.Ajax'])
+var PsychicSource = angular.module('PsychicSource', ['ionic','ionic.utils','ngCordova','PsychicSource.Authentication', 'PsychicSource.Summary', 'PsychicSource.Ajax','PsychicSource.Filters'])
 //.run(function(PushProcessingService) {
 //run once for the app
 //PushProcessingService.initialize();

6c246d05f6ae86521ab1e65f117fc764c32a46a3
Efren Carbajal <efren@novotorium.com>
2016-04-04 17:39:21 -0600

    moved android senderId variable to config service

--- www/js/services/push.js
+++ www/js/services/push.js
@@ -1,15 +1,17 @@
 angular.module('PsychicSource.Push', [])
-.factory('PushNotificationService',function($ionicPopup,$state){
+.factory('PushNotificationService',function($ionicPopup,$state,ConfigService){
   var push = {
+    badgeCount: 0,
     init: function() {
       var notificationHandler = PushNotification.init({
         android: {
-            senderID: "117405771847"
+            senderID: ConfigService.gcmApiKey
         },
         ios: {
             alert: "true",
             badge: "true",
-            sound: "true"
+            sound: "true",
+            clearBadge: true
         }
       });
 
@@ -20,13 +22,27 @@ angular.module('PsychicSource.Push', [])
             // data.sound,
             // data.image,
             // data.additionalData
-          var alertPopup = $ionicPopup.alert({
-            title: data.title,
-            template: data.message
-          });
-          $state.go('app.member-home'); 
-            
+        
+        var pendingToRead = (push.badgeCount > 0) ? (push.badgeCount + 1) : 1;
+        push.badgeCount = pendingToRead;
+
+        var alertPopup = $ionicPopup.alert({
+          title: data.title,
+          template: data.message
         });
+        $state.go('app.member-home'); 
+
+        notificationHandler.setApplicationIconBadgeNumber(function() {
+             // console.log('success');
+        }, function() {
+             // console.log('error');
+        }, pendingToRead);
+
+        notificationHandler.finish(function() {
+          // console.log('finish successfully called');
+        });
+          
+      });
 
       return notificationHandler; 
     }

e8c545fb986e19a86019b915ed7751543dd22c1d
Diego Michel Rubio Ramirez <diego.rubio@novotorium.com>
2016-05-16 13:28:23 -0500

    Add script to generate www for production internal testing

--- scripts/prepare_phonegap.sh
+++ scripts/prepare_phonegap.sh
@@ -3,6 +3,11 @@ create_www_zip()
   rm www.zip
   vim www/js/services/config.js -c "%s/var environment = 'production'/var environment = '$app_environment'/g | x" &&
   vim www/config.xml -c "%s/PsychicSource Test/$app_name/g | x" &&
+  if [ -z ${app_description+x} ]; then
+    echo "Using default description"
+  else
+    vim www/config.xml -c "%s/Official PsychicSource Mobile App/$app_description/g | x"
+  fi
   cd www &&
   zip -r ../www.zip * &&
   cd .. &&

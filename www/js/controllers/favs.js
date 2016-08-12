1f26e74917c2682ff12562dcda183d3b037b55a1
Diego Michel Rubio Ramirez <diegomichel@gmail.com>
2016-08-11 20:11:46 -0600

    #50 Adds dynamic class for phone/IM availabity

--- www/js/controllers/favs.js
+++ www/js/controllers/favs.js
@@ -4,9 +4,20 @@ PsychicSource.controller('FavsCtrl',function($scope, FavsService, ConfigService)
 
   $scope.refresh = function(){
     $scope.favs = FavsService.all();
-  }
+  };
+
   $scope.remove = function(index){
     FavsService.remove(index);
-  }
+  };
+
+  $scope.availableIM = function(fav){
+    var css_class = fav.IsOnIM === true ? 'font-green' : 'font-red';
+    return css_class;
+  };
+  $scope.availablePhone = function(fav){
+    var css_class = fav.IsOnPhone === true ? 'font-green' : 'font-red';
+    return css_class;
+  };
+
 });
 

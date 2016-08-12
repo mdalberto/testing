6ac33bebb485fa61c5e2d378a73e317a803d2f1b
Diego Michel Rubio Ramirez <diegomichel@gmail.com>
2016-08-11 19:29:49 -0600

    Basic remove functionality, some fake data and visual improvements

--- www/js/controllers/favs.js
+++ www/js/controllers/favs.js
@@ -1,4 +1,9 @@
-PsychicSource.controller('FavsCtrl',function($scope, FavsService){
+PsychicSource.controller('FavsCtrl',function($scope, FavsService, ConfigService){
+  $scope.imagePath = ConfigService.assetUrlImages;
+  $scope.favs = FavsService.all();
 
+  $scope.remove = function(index){
+    FavsService.remove(index);
+  }
 });
 
--- www/js/services/favs.js
+++ www/js/services/favs.js
@@ -1,9 +1,70 @@
 //angular.module('starter.services', [])
 angular.module('PsychicSource.Favs', [])
 .factory('FavsService', function(){
+  var favs= [
+    {
+    "FavoriteAdvisorId":4704,
+    "isFavoriteActive":true,
+    "AdvisorId":289,
+    "AdvisorName":"Stella",
+    "AdvisorExtension":7343,
+    "AdvisorPhoto":"7343.jpg",
+    "IsLogIn":false,
+    "Status":"Not Logged In",
+    "IsOnIM":false,
+    "IMStatusID":0,
+    "IsOnPhone":true,
+    "LogInStatusID":7,
+    "IsPhoneLogIn":false,
+    "OnPhoneConference":false,
+    "CallBackStatusID":1,
+    "ServiceAvailable":1
+    },
+    {
+    "FavoriteAdvisorId":4704,
+    "isFavoriteActive":true,
+    "AdvisorId":289,
+    "AdvisorName":"Bonnie",
+    "AdvisorExtension":7982,
+    "AdvisorPhoto":"7982.jpg",
+    "IsLogIn":false,
+    "Status":"Not Logged In",
+    "IsOnIM":false,
+    "IMStatusID":0,
+    "IsOnPhone":true,
+    "LogInStatusID":7,
+    "IsPhoneLogIn":false,
+    "OnPhoneConference":false,
+    "CallBackStatusID":1,
+    "ServiceAvailable":1
+    },
+    {
+    "FavoriteAdvisorId":4704,
+    "isFavoriteActive":true,
+    "AdvisorId":289,
+    "AdvisorName":"Penelope",
+    "AdvisorExtension":7710,
+    "AdvisorPhoto":"7710.jpg",
+    "IsLogIn":false,
+    "Status":"Not Logged In",
+    "IsOnIM":false,
+    "IMStatusID":0,
+    "IsOnPhone":true,
+    "LogInStatusID":7,
+    "IsPhoneLogIn":false,
+    "OnPhoneConference":false,
+    "CallBackStatusID":1,
+    "ServiceAvailable":1
+    },
+  ];
   return {
     all: function(){
-      return 1;
+      return favs;
+    },
+    remove: function(fav) {
+      favs.splice(favs.indexOf(fav), 1);
+      //Make call to delete from server
+      //Fetch from server and redraw
     }
   }
 })

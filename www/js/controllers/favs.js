2bc6d214db4c706bdfe59765a76f129e84cb4a9a
Diego Michel Rubio Ramirez <diegomichel@gmail.com>
2016-08-11 22:56:53 -0600

    #50 Fetches favorites from server

--- www/js/controllers/favs.js
+++ www/js/controllers/favs.js
@@ -1,11 +1,20 @@
-PsychicSource.controller('FavsCtrl',function($scope, FavsService, ConfigService){
+PsychicSource.controller('FavsCtrl',function($scope, FavsService, ConfigService, AjaxService, AuthService, $ionicLoading){
   $scope.imagePath = ConfigService.assetUrlImages;
   $scope.profileUrl = ConfigService.profileUrl;
-  $scope.favs = FavsService.all();
-
   $scope.refresh = function(){
-    $scope.favs = FavsService.all();
+    //$ionicLoading.show({template: 'Loading Favorite Advisors...'});
+    AjaxService.getFavorites(AuthService.id(),$scope.preferences).then(function(res){
+      $ionicLoading.hide();
+      $scope.favs = res.data;
+    }, function(error){
+      //$ionicLoading.hide();
+      var alertPopup = $ionicPopup.alert({
+        title: 'Update operation failed! (2)',
+        template: 'Please verify you are connected to the internet'
+      });
+    });
   };
+  $scope.refresh();
 
   $scope.remove = function(index){
     FavsService.remove(index);

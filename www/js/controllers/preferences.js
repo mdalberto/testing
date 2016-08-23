37563379dc025cce21489a3f83ea4876c2bb51aa
Diego Michel Rubio Ramirez <diegomichel@gmail.com>
2016-08-23 08:34:36 -0600

    #58 makes use of Popup factory

--- www/js/controllers/preferences.js
+++ www/js/controllers/preferences.js
@@ -1,23 +1,22 @@
-PsychicSource.controller('PreferencesCtrl',function($scope, $ionicLoading, $ionicPopup, AuthService, PreferencesService, preferences,AjaxService){
+PsychicSource.controller('PreferencesCtrl',function($scope, $ionicLoading, AuthService, PreferencesService, preferences, AjaxService, Popup){
  $scope.preferences = preferences;
- 
+
  $scope.preferenceUpdated = function($index){
   $ionicLoading.show({template: 'Updating Setting...'});
   AjaxService.savePreferences(AuthService.id(),$scope.preferences).then(function(res){
     PreferencesService.getPreferences().then(function(preferences){
-      $ionicLoading.hide(); 
-      $scope.preferences = preferences;
-    }, function(err2){                                       
       $ionicLoading.hide();
-      var alertPopup = $ionicPopup.alert({
+      $scope.preferences = preferences;
+    }, function(err2){
+      $ionicLoading.hide();
+      Popup.show('alert', {
         title: 'Update operation failed! (2)',
         template: 'Please verify you are connected to the internet'
       });
-                                             
     });
   },function(err){
     $ionicLoading.hide();
-    var alertPopup = $ionicPopup.alert({
+    Popup.show('alert', {
       title: 'Update operation failed!',
       template: 'Please verify you are connected to the internet'
     });

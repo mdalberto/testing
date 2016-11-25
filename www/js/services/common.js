87498d444a9402f10672958ed8e368b36a67e9e7
Diego Michel Rubio Ramirez <diegomichel@gmail.com>
2016-11-25 07:37:19 -0700

    #73 Common functions to track links and phone calls

--- www/js/services/common.js
+++ www/js/services/common.js
@@ -1,5 +1,5 @@
 angular.module('PsychicSource.Common', [])
-.factory('CommonService', function($q, $http, AjaxService){
+.factory('CommonService', function($q, $http, AjaxService, GTM){
   var commonHandler = {
     minimumBalance: null,
     callNowNumber: function(){
@@ -8,9 +8,18 @@ angular.module('PsychicSource.Common', [])
       else
         return "8668842981";
     },
-   setMinimumBalance: function(data) {
+    setMinimumBalance: function(data) {
       commonHandler.minimumBalance = parseFloat(data.data.minPurchasedBalance).toPrecision();
-    }
+    },
+    externalNoPrompt: function($event){
+      GTM.trackEvent('LinkNoPrompt', 'open', $event.currentTarget.dataset.href, 1);
+      window.open($event.currentTarget.dataset.href,'_system','location=yes');
+    },
+    callNow: function(){
+      GTM.trackEvent('Calls', 'call', this.callNowNumber, 1);
+      window.open('tel:' + this.callNowNumber, '_system', 'location=yes');
+      return false;
+    },
   };
   return commonHandler;
 });
